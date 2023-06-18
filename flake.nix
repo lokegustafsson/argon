009e06574729fbd240fb92df24351b6e5de611bf
Loke Gustafsson <lokegustafsson@pm.me>
2023-06-18 17:10:14 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    cargo2nix = {
+      url = "github:cargo2nix/cargo2nix";
+      inputs.rust-overlay.follows = "rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, cargo2nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ cargo2nix.overlays.default ];
+          config.allowUnfree = false;
+        };
+        lib = nixpkgs.lib;
+        rust = import ./rust.nix {
+          inherit lib pkgs;
+          extra-overrides = { mkNativeDep, mkEnvDep, mkRpath, mkOverride, p }:
+            [ (mkNativeDep "argon" [ ]) ];
+        };
+      in {
+        devShells.default = rust.workspaceShell {
+          packages = let p = pkgs;
+          in [
+            cargo2nix.outputs.packages.${system}.cargo2nix
+            p.rust-bin.stable.latest.clippy
+            p.rust-bin.stable.latest.default
+          ];
+        };
+
+        LD_LIBRARY_PATH = let p = pkgs; in lib.makeLibraryPath [ ];
+
+        packages.default = rust.argon;
+      });
+}
---
+++ rust.nix
@@ -0,0 +1,72 @@
+{ lib, pkgs, extra-overrides, use-mold ? true }:
+let
+  rustPkgs = pkgs.rustBuilder.makePackageSet {
+    rustVersion = "latest";
+    packageFun = import ./Cargo.nix;
+    packageOverrides = p:
+      let
+        mkNativeDep = cratename: extra-deps:
+          p.rustBuilder.rustLib.makeOverride {
+            name = cratename;
+            overrideAttrs = drv: {
+              propagatedBuildInputs = (drv.propagatedBuildInputs or [ ])
+                ++ extra-deps;
+            };
+          };
+        mkEnvDep = cratename: env-deps:
+          (p.rustBuilder.rustLib.makeOverride {
+            name = cratename;
+            overrideAttrs = drv:
+              (env-deps // {
+                propagatedBuildInputs = (drv.propagatedBuildInputs or [ ]) ++ [
+                  (p.rustBuilder.overrides.patches.propagateEnv cratename
+                    (lib.attrsets.mapAttrsToList
+                      (name: value: { inherit name value; }) env-deps))
+                ];
+              });
+          });
+        mkRpath = cratename: libs:
+          p.rustBuilder.rustLib.makeOverride {
+            name = cratename;
+            overrideAttrs = drv: {
+              postFixup = let libPath = lib.makeLibraryPath libs;
+              in ''
+                patchelf --add-rpath "${libPath}" $out/bin/${cratename}
+                patchelf --add-rpath "${libPath}" $bin/bin/${cratename}
+              '';
+            };
+          };
+        mkOverride = cratename: overrideAttrs:
+          p.rustBuilder.rustLib.makeOverride {
+            name = cratename;
+            inherit overrideAttrs;
+          };
+      in (if use-mold then
+        [
+          (p.rustBuilder.rustLib.makeOverride {
+            overrideArgs = old: {
+              rustcLinkFlags = (old.rustcLinkFlags or [ ]) ++ [
+                "-C"
+                "linker=${pkgs.clang}/bin/clang"
+                "-C"
+                "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"
+              ];
+            };
+          })
+        ]
+      else
+        [ ]) ++ [
+          (p.rustBuilder.rustLib.makeOverride {
+            registry = "registry+https://github.com/rust-lang/crates.io-index";
+            overrideArgs = old: {
+              rustcLinkFlags = (old.rustcLinkFlags or [ ])
+                ++ [ "--cap-lints" "warn" ];
+            };
+          })
+        ] ++ (extra-overrides {
+          inherit mkNativeDep mkEnvDep mkRpath mkOverride p;
+        });
+  };
+in (builtins.mapAttrs (crate: f: f { }) rustPkgs.workspace) // {
+  inherit (rustPkgs) workspaceShell;
+}
