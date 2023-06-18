288acc550019bc20a1c2d29e7c34320799d3dd7b
Loke Gustafsson <lokegustafsson@pm.me>
2023-06-18 19:28:03 +0100

    remove copy pasted template stuff

--- src/main.rs
+++ src/main.rs
@@ -19,9 +19,6 @@ struct Args {
 fn main() -> ExitCode {
     tracing::subscriber::set_global_default(
         Targets::new()
-            .with_target("h2", tracing::Level::INFO)
-            .with_target("tokio_util::codec::framed_impl", tracing::Level::DEBUG)
-            .with_target("eframe::native::run", tracing::Level::DEBUG)
             .with_default(tracing::Level::TRACE)
             .with_subscriber(
                 tracing_subscriber::FmtSubscriber::builder()
